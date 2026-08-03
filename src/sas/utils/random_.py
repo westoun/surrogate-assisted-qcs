7a75d0673ece8d078e39f14361bdc85f643ba3a0
Christoph Stein <christoph.werner.stein@gmail.com>
2026-08-03 14:59:03 +0200

    Remove unneeded utils function

--- src/sas/utils/random_.py
+++ src/sas/utils/random_.py
@@ -5,14 +5,7 @@ from .circuit import Circuit
 from .gates import Gate, H, S, T, CX
 
 
-def random_gate(qubit_num: int, uniform_configuration_choice: bool = True) -> Gate:
-    if uniform_configuration_choice:
-        return _random_gate_uniform_by_config(qubit_num)
-    else:
-        return _random_gate_uniform_by_gatetype(qubit_num)
-
-
-def _random_gate_uniform_by_config(qubit_num: int) -> Gate:
+def random_gate(qubit_num: int) -> Gate:
     GateTypes = [
         H, S, T, CX
     ]
@@ -21,21 +14,6 @@ def _random_gate_uniform_by_config(qubit_num: int) -> Gate:
     ]
 
     GateType = choices(population=GateTypes, weights=weights, k=1)[0]
-
-    if GateType == CX:
-        control, target = sample(range(0, qubit_num), 2)
-        return CX(control=control, target=target)
-    else:
-        target = randint(0, qubit_num - 1)
-        return GateType(target)
-
-
-def _random_gate_uniform_by_gatetype(qubit_num: int) -> Gate:
-    GateTypes = [
-        H, S, T, CX
-    ]
-
-    GateType = choice(GateTypes)
 
     if GateType == CX:
         control, target = sample(range(0, qubit_num), 2)
