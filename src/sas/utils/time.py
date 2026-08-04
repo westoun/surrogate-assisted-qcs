1420e9db70e293ac3ba08499021bc5f6b36f691e
Christoph Stein <christoph.werner.stein@gmail.com>
2026-08-04 15:41:27 +0200

    Remove unneeded method

--- src/sas/utils/time.py
+++ src/sas/utils/time.py
@@ -1,14 +1,6 @@
 from datetime import datetime
 
 
-def duration_to_seconds(duration: str) -> float:
-    hours = int(duration.split(":")[0])
-    minutes = int(duration.split(":")[1])
-    seconds = float(duration.split(":")[2])
-
-    return seconds + minutes * 60 + hours * 60 * 60
-
-
 def get_timestamp() -> str:
     return str(datetime.now())
 
