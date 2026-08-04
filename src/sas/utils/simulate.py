da1600e3d472a4d7cbaeba5a491f985588c6dfe2
Christoph Stein <christoph.werner.stein@gmail.com>
2026-08-04 15:01:33 +0200

    Implement qiskit unitary simulation

--- src/sas/utils/simulate.py
+++ src/sas/utils/simulate.py
@@ -1,12 +1,29 @@
 import numpy as np
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import Operator
 
 from .circuit import Circuit
-
+from .gates import H, S, T, CX
 
 def simulate_unitary(circuit: Circuit) -> np.ndarray:
+    qiskit_circuit = circuit_to_qiskit(circuit)
+    unitary = Operator(qiskit_circuit).data
+    return unitary
 
-    # translate circuit gates to simulator format.
+def circuit_to_qiskit(circuit: Circuit) -> QuantumCircuit:
+    qiskit_circuit = QuantumCircuit(circuit.qubit_num)
 
-    # derive and return unitary.
+    for gate in circuit.gates:
+        if type(gate) == H:
+            qiskit_circuit.h(gate.target) 
+        elif type(gate) == S:
+            qiskit_circuit.s(gate.target) 
+        elif type(gate) == T:
+            qiskit_circuit.t(gate.target) 
+        elif type(gate) == CX:
+            qiskit_circuit.cx(gate.control, gate.target) 
+        else:
+            raise NotImplementedError(f"No mapping found for gate type '{type(gate)}'")
 
-    raise NotImplementedError()
+    return qiskit_circuit
+     
