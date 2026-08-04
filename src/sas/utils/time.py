f97fb22006e72d9bab3491f29faf8ef69ddf3e16
Christoph Stein <christoph.werner.stein@gmail.com>
2026-08-04 15:47:46 +0200

    Add aggregation flag to time recorder

--- src/sas/utils/time.py
+++ src/sas/utils/time.py
@@ -6,13 +6,23 @@ def get_timestamp() -> str:
 
 
 class TimeRecorder():
+    aggregate: bool
+    duration: float
+
+    def __init__(self, aggregate: bool = False):
+        self.aggregate = aggregate
+        self.duration = 0.0
 
     def __enter__(self):
+        if not self.aggregate:
+            self.duration = 0.0
+
         self.start = datetime.now()
 
     def __exit__(self, exc_type, exc, tb):
-        self.end = datetime.now()
+        self.duration = (datetime.now() - self.start).total_seconds()
+        self.start = None
 
-    @property
-    def duration(self) -> float:
-        return (self.end - self.start).total_seconds()
+    def reset(self) -> None:
+        self.start = None
+        self.duration = 0.0
