37ec1aa0e4a423e24a669f8f969fea807f8818ca
Christoph Stein <christoph.werner.stein@gmail.com>
2026-08-10 13:19:54 +0200

    Add logging of total duration

--- src/sas/ea/logging_.py
+++ src/sas/ea/logging_.py
@@ -20,7 +20,7 @@ def log_epoch_results(generation: int, population: List[Circuit],
 
         if add_header:
             header = "generation; fitness_best; fitness_median; fitness_mean; fitness_stdev; "
-            header += "ea_duration; eval_duration; train_duration; pred_duration; "
+            header += "ea_duration; eval_duration; train_duration; pred_duration; total_duration; "
             header += "ea_memory; eval_memory; train_memory; pred_memory; "
             header += "fitness_mse; rank_correlation"
             target_file.write(header + "\n")
@@ -29,8 +29,10 @@ def log_epoch_results(generation: int, population: List[Circuit],
             circuit.fitness for circuit in population
         ]
 
+        total_duration = ea_duration + eval_duration + train_duration + pred_duration
+
         line = f"{generation}; {min(fitness_scores)}; {median(fitness_scores)}; {mean(fitness_scores)}; {stdev(fitness_scores)}; "
-        line += f"{ea_duration}; {eval_duration}; {train_duration}; {pred_duration}; "
+        line += f"{ea_duration}; {eval_duration}; {train_duration}; {pred_duration}; {total_duration}; "
         line += f"{ea_memory}; {eval_memory}; {train_memory}; {pred_memory}; "
         line += f"{fitness_mse}; {rank_correlation}"
         target_file.write(line + "\n")
